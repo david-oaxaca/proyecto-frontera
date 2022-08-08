e58aa3254eb74d8ee2412ed1ab0d1a07aff911d9
David Oaxaca <tdwdavid76@gmail.com>
2022-08-07 20:35:19 -0500

    Minor changes added

---
+++ src/Components/Drawer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Drawer() {
+  return (
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Proyecto Frontera</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                    <a className="nav-link" href="/">Inicio</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/Analisis">Analisis</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/Galeria">Galeria</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/Rutas">Rutas</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
