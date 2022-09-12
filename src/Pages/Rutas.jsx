0ca37a0dc66073fc5057809191bfbd79a22542d9
David Oaxaca <tdwdavid76@gmail.com>
2022-09-11 20:40:40 -0500

    Minor styles changes on homepage

--- src/Pages/Rutas.jsx
+++ src/Pages/Rutas.jsx
@@ -1,34 +1,7 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; /* eslint import/no-webpack-loader-syntax: off */
+import React from 'react';
 import '../Assets/Styles/rutas.scss'
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWQtb2F4YWNhIiwiYSI6ImNsNnR5ZWdpajB2cmgzbW1zcDltbHBnNm4ifQ.ctBACt6TJ1BEDeLlWZArJA';
-
 export default function Rutas() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-99.23);
-  const [lat, setLat] = useState(19.58);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-      map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
-  });
-  
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
 
   return (
     <main>
@@ -46,7 +19,6 @@ export default function Rutas() {
           </nav>
           </div>
           
-          <div ref={mapContainer} className="map-container" />
         </div>
       </section>
     </main>
