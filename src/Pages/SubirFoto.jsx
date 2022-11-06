2ad9f2d0323e9bb522259fabee31286db96c776e
David Oaxaca <tdwdavid76@gmail.com>
2022-11-06 15:02:56 -0600

    Header and Footer modified

--- src/Pages/SubirFoto.jsx
+++ src/Pages/SubirFoto.jsx
@@ -4,7 +4,7 @@ import '../Assets/Styles/style.scss';
 
 export default function SubirFoto() {
   return (
-    <div className='main-bg'>
+    <div className='main-bg-center'>
       <Formulario/>
     </div>
   )
